6e149b0e66b650d7f6b6c935396e19f221574074
Roman <AverkievRE@yandex.ru>
2022-04-27 14:34:18 +0300

    Realizacion project Rectangle to Task_1_1

--- Task_1_1/Rectangle/Program.cs
+++ Task_1_1/Rectangle/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+  Написать программу, которая определяет площадь прямоугольника со сторонами a и b. Если
+пользователь вводит некорректные значения (отрицательные или ноль), должно выдаваться
+сообщение об ошибке. Возможность ввода пользователем строки вида «абвгд» или нецелых чисел
+игнорировать.
+ */
+
+using System;
+
+namespace Rectangle
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длинну прямоугольника");
+            string? str = Console.ReadLine();
+
+            int length;
+            int.TryParse(str, out length);
+            if (length <= 0)
+            {
+                Console.WriteLine("Неправильный ввод длинны");
+                return;
+            }
+
+            Console.WriteLine("Введите ширину прямоугольника");
+            str = Console.ReadLine();
+
+            int width;
+            int.TryParse(str, out width);
+            if(width <= 0)
+            {
+                Console.WriteLine("Неправильный ввод ширины");
+                return;
+            }
+
+            Console.WriteLine($"Площадь прямоугольника: {length * width}");
+
+        }
+
+    }
+}
