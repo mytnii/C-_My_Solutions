c6e01712abfa7beb99eecfb67e1b7e8b6a2757db
Roman <AverkievRE@yandex.ru>
2022-04-27 19:57:58 +0300

    Realization project AnotherTriangle to Task_1_1

--- Task_1_1/AnotherTriangle/Program.cs
+++ Task_1_1/AnotherTriangle/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+  Написать программу, которая запрашивает с клавиатуры число N и выводит на экран следующее
+«изображение», состоящее из N строк:
+
+
+
+
+        *
+       ***
+      *****
+     *******
+    *********
+   ***********
+  *************
+ */
+
+using System;
+
+namespace AnotherTriangle
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество строк");
+            string? str = Console.ReadLine();
+
+            int value;
+            int.TryParse(str, out value);
+            
+            if(value <= 0)
+            {
+                Console.WriteLine("Неверный ввод");
+
+                return;
+            }
+
+            for(int i = 0; i < value; i++)
+            {
+                for(int j = 0; j < value * 2; j++)
+                {
+                  if(j >= value  - i && j <= value + i)
+                        Console.Write('*');
+                  else
+                        Console.Write(' ');
+                }
+                    Console.WriteLine();
+            }
+        }
+    }
+}
