14b08c0e1d3f8ac4766cd625787f97afc5d10a5f
Roman <AverkievRE@yandex.ru>
2022-04-29 18:35:57 +0300

    Realizacion project NoPositive to Task_1_1

--- Task_1_1/NoPositive/Program.cs
+++ Task_1_1/NoPositive/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+   Написать программу, которая заменяет все положительные элементы в трёхмерном массиве на нули.
+  Число элементов в массиве и их тип определяются разработчиком.
+ */
+
+using System;
+
+namespace NoPositive
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 3;
+            int[,,] matrix = new int[size, size, 5];
+
+            Random randomNum = new Random();
+
+            for (int i = 0; i < size; i++)
+                for (int j = 0; j < size; j++)
+                    for (int k = 0; k < 5; k++)
+                        matrix[i, j, k] = randomNum.Next(-10, 10);
+
+            for (int i = 0; i < size; ++i)
+            {
+                for (int j = 0; j < size; ++j)
+                {
+                    for (int k = 0; k < 5; k++)
+                        Console.Write(matrix[i, j, k] + "\t");
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = 0; i < size; ++i)
+            {
+                for (int j = 0; j < size; ++j)
+                {
+                    for (int k = 0; k < 5; k++)
+                    {
+                        if (matrix[i, j, k] > 0)
+                            matrix[i, j, k] = 0;
+                    }
+                }
+            }
+            for (int i = 0; i < size; ++i)
+            {
+                for (int j = 0; j < size; ++j)
+                {
+                    for (int k = 0; k < 5; k++)
+                        Console.Write(matrix[i, j, k] + "\t");
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
