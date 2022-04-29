5549c549baa56b88df3497e50959a33680bca8fc
Roman <AverkievRE@yandex.ru>
2022-04-29 18:45:56 +0300

    Realizacion project Non_NegativeSum to Task_1_1

--- Task_1_1/Non_NegativeSum/Program.cs
+++ Task_1_1/Non_NegativeSum/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+   Написать программу, которая определяет сумму неотрицательных элементов в одномерном массиве.
+   Число элементов в массиве и их тип определяются разработчиком.
+ */
+
+using System;
+
+namespace Non_NegativeSum
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 5;
+            int[] arr = new int[size];
+
+            Random random = new Random();
+
+            for(int i = 0; i < size; i++)
+            {
+                arr[i] = random.Next(-10, 10);
+            }
+
+            foreach(int i in arr)
+            {
+                Console.Write(i + "\t");
+            }
+            Console.WriteLine();
+
+            int sumPositiveNumber = 0;
+
+            foreach(int i in arr)
+                if(i> 0)
+                    sumPositiveNumber += i;
+
+            Console.WriteLine(sumPositiveNumber);
+        }
+    }
+}
