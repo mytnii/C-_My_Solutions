5dc28e24222e006e24292171aded086cfb880065
Roman <AverkievRE@yandex.ru>
2022-04-30 08:32:48 +0300

    Realizacion project Doubler to Task_1_2

--- Task_1_2/Doubler/Program.cs
+++ Task_1_2/Doubler/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+   Напишите программу, которая удваивает в первой введённой строке все символы, принадлежащие
+второй введённой строке.
+
+Пример:
+
+ВВОД 1: написать программу, которая
+ВВОД 2: описание
+ВЫВОД: ннааппииссаать ппроограамму, коотоораая
+ */
+
+using System;
+
+namespace Doubler
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первую строку");
+            string? str = Console.ReadLine();
+
+            Console.WriteLine("Введите вторую строку");
+            string? str2 = Console.ReadLine();
+
+            bool symbol;
+
+            if (str != null && str2 != null)
+                for (int i = 0; i < str.Length; i++)
+                {
+                    symbol = false;
+
+                    for(int j = 0; j < str2.Length; j++)
+                    {
+                        if(str[i] == str2[j])
+                        {
+                            symbol = true;
+                        }
+                        
+
+
+                    }
+
+                    if (symbol)
+                        Console.Write($"{str[i]}{str[i]}");
+                    else
+                        Console.Write(str[i]);
+                }
+        }
+    }
+}
